386f26dd5395e43a889739ba21b0e292e700e70d
yop <aaa@yopmail.com>
2015-08-01 01:36:07 +0900

    Packet.S2C/C2S 테스트 추가

--- src/MeronaTest/Utilities/Packet.Descriptors.cs
+++ src/MeronaTest/Utilities/Packet.Descriptors.cs
@@ -26,10 +26,22 @@ namespace MeronaTest.Utilities
             public string name;
         }
 
+        public class Login : Packet
+        {
+            [C2S]
+            public string id;
+            [C2S]
+            public string password;
+
+            [S2C]
+            public bool result;
+        }
+
         [TestMethod]
         public void KeyOf()
         {
-            Assert.IsTrue(
+            Assert.AreEqual(
+                1,
                 Packet.GetKeyFields<QueryPlayer>()
                 /* Tuple<String, FieldInfo>
                  *    item1 : 타겟 Model의 클래스 이름
@@ -37,7 +49,42 @@ namespace MeronaTest.Utilities
                  */
                 .Where(field => field.Item1 == "Player")
                 .Where(field => field.Item2.Name == "player_id")
-                .Count() == 1);
+                .Count());
+        }
+
+        [TestMethod]
+        public void C2S()
+        {
+            Assert.AreEqual(
+                2,
+                Packet.GetC2SFields<Login>()
+                .Count());
+
+            Assert.AreEqual(
+                1,
+                Packet.GetC2SFields<Login>()
+                .Where(field => field.Name == "id")
+                .Count());
+
+            Assert.AreEqual(
+                1,
+                Packet.GetC2SFields<Login>()
+                .Where(field => field.Name == "password")
+                .Count());
+        }
+        [TestMethod]
+        public void S2C()
+        {
+            Assert.AreEqual(
+                1,
+                Packet.GetS2CFields<Login>()
+                .Count());
+
+            Assert.AreEqual(
+                1,
+                Packet.GetS2CFields<Login>()
+                .Where(field => field.Name == "result")
+                .Count());
         }
     }
 }
