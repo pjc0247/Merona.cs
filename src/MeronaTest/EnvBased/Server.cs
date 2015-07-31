cad3f88969b4e11b2a194647e8acb31279652b75
yop <aaa@yopmail.com>
2015-08-01 02:38:51 +0900

    ServerBased Test

---
+++ src/MeronaTest/EnvBased/Server.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MeronaTest.EnvBased
+{
+    [TestClass]
+    public class ServerTest : ServerBasedTest
+    {
+        
+        [TestMethod]
+        public void IsRunning()
+        {
+            Assert.IsTrue(
+                server.isRunning);
+        }
+
+        [TestMethod]
+        public void TS()
+        {
+
+        }
+    }
+}
---
+++ src/MeronaTest/EnvBased/ServerBasedTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Merona;
+
+namespace MeronaTest.EnvBased
+{
+    public class ServerBasedTest
+    {
+        protected Server server { get; set; }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            server = new Server("TestServer");
+
+            server.Start();
+
+            Console.WriteLine("Initialize");
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            server.Kill();
+
+            Console.WriteLine("Cleanup");
+        }
+    }
+}
