946c87e3573fbcd97d5dd726831a6dc57f1d3c67
yop <aaa@yopmail.com>
2015-08-02 02:08:54 +0900

    Config

--- src/Merona/Config.cs
+++ src/Merona/Config.cs
@@ -9,16 +9,34 @@ namespace Merona
 {
     public class Config
     {
+        public int schedulerMaxInterval { get; set; }
+        public int sessionRingBufferSize { get; set; }
+        public int sessionRecvBufferSize { get; set; }
+
+        public String dbHostName { get; set; }
+        public String dbDatabaseName { get; set; }
+
+        public String endian { get; set; }
+
+        /// <summary>
+        /// 기본 설정
+        /// </summary>
         public Config()
         {
-            /*
-             * scheduler_max_interval
-             * db_host
-             * db_name
-             * session_ringbuffer_size
-             * session_recvbuffer_size
-             * endian
-             */
+            this.schedulerMaxInterval = 30;
+            this.sessionRecvBufferSize = 128;
+            this.sessionRingBufferSize = 1024;
+            this.dbHostName = "localhost";
+            this.dbDatabaseName = "test";
+            this.endian = "little"; // 기본값 big?
+        }
+        /// <summary>
+        /// 파일로부터 설정값 생성
+        /// </summary>
+        /// <param name="path">설정 파일 경로</param>
+        public Config(String path)
+        {
+            throw new NotImplementedException();
         }
     }
 }
