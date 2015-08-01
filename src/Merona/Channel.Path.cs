dcec0ef6dd95cfb5b7c4531b3bd887cd2fc09112
yop <aaa@yopmail.com>
2015-08-01 22:54:49 +0900

    Channel에 isFixed 추가

--- src/Merona/Channel.Path.cs
+++ src/Merona/Channel.Path.cs
@@ -61,7 +61,7 @@ namespace Merona
                     return new DynamicPathMaker();
                 }
             }
-            private static int[] ToRawPath(string path)
+            private static int[] ToRawPath(string path, ref bool isFixed)
             {
                 var tokens = path.Split('.');
                 var raw = new int[tokens.Length];
@@ -69,6 +69,10 @@ namespace Merona
                 for (var i = 0; i < tokens.Length; i++)
                 {
                     raw[i] = tokens[i].GetHashCode();
+
+                    if (raw[i] == asterisk ||
+                       raw[i] == doubleAsterisk)
+                        isFixed = false;
                 }
 
                 return raw;
@@ -77,12 +81,16 @@ namespace Merona
             internal static readonly int doubleAsterisk = "**".GetHashCode();
 
             public string path { get; private set; }
+            public bool isFixed { get; private set; }
             internal int[] raw { get; private set; }
 
             public Path(string path)
             {
+                bool isFixed = true;
+
                 this.path = path;
-                this.raw = ToRawPath(path);
+                this.raw = ToRawPath(path, ref isFixed);
+                this.isFixed = isFixed;
             }
 
             public bool IsMatch(String inPath)
