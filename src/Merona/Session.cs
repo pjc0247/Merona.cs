53a37662a9504ab024f691c3ec1986b19d31d57b
yop <aaa@yopmail.com>
2015-08-02 22:38:52 +0900

    세션에서 Console이 아니라 logger로 로깅

--- src/Merona/Session.cs
+++ src/Merona/Session.cs
@@ -111,7 +111,7 @@ namespace Merona
             }
             catch (InternalBufferOverflowException e)
             {
-                Console.WriteLine("ringbuffer overflow");
+                server.logger.Warn("ringbuffer overflow");
             }
             finally
             {
