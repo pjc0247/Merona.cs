fe380892d3c3d8c2a111011f22b3c61047c0af6a
pjc02478 <pjc0247@gmail.com>
2015-08-03 10:43:26 +0900

    SchedulerTest 추가

---
+++ src/MeronaTest/EnvBased/Scheduler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Merona;
+
+namespace MeronaTest.EnvBased
+{
+    [TestClass]
+    public class SchedulerTest : ServerBasedTest
+    {
+        [TestMethod]
+        public void IsSafeThread()
+        {
+            object obj = new object();
+            bool isSafeThread = false;
+
+            server.scheduler.Defer(() =>
+            {
+                isSafeThread = Server.isSafeThread;
+                lock (obj)
+                {
+                    Monitor.Pulse(obj);
+                }
+            });
+
+            lock (obj)
+            {
+                Monitor.Wait(obj);
+            }
+
+            Assert.IsTrue(
+                isSafeThread);
+        }
+    }
+}
