0e2b65670f36d8fbd751f5d22cda9aa508f1281d
pjc02478 <pjc0247@gmail.com>
2015-08-03 12:03:11 +0900

    Server.Worker를 WorkerBasedClass기반으로 재구현

--- src/Merona/Server.Worker.cs
+++ src/Merona/Server.Worker.cs
@@ -11,13 +11,12 @@ namespace Merona
 {
     public sealed partial class Server
     {
-        private class Worker
+        private class Worker : WorkerBasedClass
         {
             private Server server { get; set; }
-            private Thread thread { get; set; }
 
             private bool isWorkerInitialized { get; set; }
-            private bool isQuitReserved { get; set; }
+            private CancellationTokenSource cts;
 
             public int ThreadId {
                 get{
@@ -28,84 +27,53 @@ namespace Merona
             public Worker(Server server)
             {
                 this.server = server;
-                this.thread = new Thread(Loop);
             }
 
-            public void Start()
-            {
-                server.logger.Info("Worker::Start");
-
-                thread.Start();
-
-                server.logger.Debug("Worker::Start::SpinWait isWorkerInitialized");
-
-                /* blocking wait */
-                SpinWait.SpinUntil(() =>
-                {
-                    return isWorkerInitialized;
-                });
-
-                server.logger.Debug("Worker::Start::EndSpinWait isWorkerInitialized");
-            }
-            public void Kill()
-            {
-                isQuitReserved = true;
-
-                server.logger.Info("Worker::Kill");
-                server.logger.Debug("Worker::Kill::SpinWait thread.isAlive");
-
-                /* blocking wait */
-                SpinWait.SpinUntil(() =>
-                {
-                    return !thread.IsAlive;
-                });
-
-                server.logger.Debug("Worker::EndSpinWait");
-            }
-            private void Loop(object arg)
+            protected override void Setup()
             {
                 /* initialize thread-locals */
                 Server.current = server;
                 Scheduler.current = server.scheduler;
                 Session.current = new Session(); //for test
 
-                var cts = new CancellationTokenSource();
+                cts = new CancellationTokenSource();
                 var t = new Timer(delegate(object _arg)
                 {
                     cts.Cancel();
                     Interlocked.Exchange(ref cts, new CancellationTokenSource());
                 }, null, 0, 30); /* TODO : config */
 
-                server.logger.Info("Worker::BeginThread tid({0})", ThreadId);
+                server.logger.Info("Worker::Setup tid({0})", ThreadId);
 
                 isWorkerInitialized = true;
-
-                while (!isQuitReserved)
-                {
-                    Event ev = null;
-                    
-                    try
-                    {
-                        ev = server.pendingEvents.Take(cts.Token);
-
-                        if (ev.type == Event.Type.Accept)
-                            OnAccept((AcceptEvent)ev);
-                        else if (ev.type == Event.Type.Disconnect)
-                            OnDisconnect((DisconnectEvent)ev);
-                        else if (ev.type == Event.Type.RecvPacket)
-                            OnRecvPacket((RecvPacketEvent)ev);
-                        else if (ev.type == Event.Type.CallFunc)
-                            OnCallFunc((CallFuncEvent)ev);
-                    }
-                    catch (OperationCanceledException e)
-                    {
-                        /* Take() operation canceled */
-                    }
-                }
-
+            }
+            protected override void Cleanup()
+            {
                 isWorkerInitialized = false;
 
-                server.logger.Info("Worker::EndThraed");
+                server.logger.Info("Worker::Cleanup");
+            }
+            protected override void WorkerRoutine()
+            {
+                Event ev = null;
+
+                try
+                {
+                    ev = server.pendingEvents.Take(cts.Token);
+
+                    if (ev.type == Event.Type.Accept)
+                        OnAccept((AcceptEvent)ev);
+                    else if (ev.type == Event.Type.Disconnect)
+                        OnDisconnect((DisconnectEvent)ev);
+                    else if (ev.type == Event.Type.RecvPacket)
+                        OnRecvPacket((RecvPacketEvent)ev);
+                    else if (ev.type == Event.Type.CallFunc)
+                        OnCallFunc((CallFuncEvent)ev);
+                }
+                catch (OperationCanceledException e)
+                {
+                    /* Take() operation canceled */
+                }
             }
 
             private void OnAccept(AcceptEvent e)
