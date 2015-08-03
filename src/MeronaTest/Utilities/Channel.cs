1f48fc97dc4c64c6d837f70eb75389be21a6f020
pjc02478 <pjc0247@gmail.com>
2015-08-03 18:30:11 +0900

    Channel.Query 테스트 추가

--- src/MeronaTest/Utilities/Channel.cs
+++ src/MeronaTest/Utilities/Channel.cs
@@ -8,5 +8,17 @@ namespace MeronaTest.Utilities
     [TestClass]
     class ChannelTest
     {
+        [TestMethod]
+        public void Query()
+        {
+            var ch = new Channel("world");
+
+            ch.Join(new Session());
+            ch.Join(new Session());
+
+            Assert.AreEqual(
+                2,
+                ch.Query().Count);
+        }
     }
 }
