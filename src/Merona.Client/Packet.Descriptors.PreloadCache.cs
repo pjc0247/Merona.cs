1f1133c5c4271152139cbb5ef257352ddaacbb0b
pjc02478 <pjc0247@gmail.com>
2015-08-04 14:32:55 +0900

    Merona.Client에 PacketId 추가

---
+++ src/Merona.Client/Packet.Descriptors.PreloadCache.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Reflection;
+
+namespace Merona.Client
+{
+    public partial class Packet
+    {
+        private static Dictionary<int, Type> types;
+
+        static Packet()
+        {
+            types = new Dictionary<int, Type>();
+
+            var packets = Assembly.GetEntryAssembly().GetTypes()
+                .Where(type => type.IsSubclassOf(typeof(Packet)));
+
+            foreach (var packet in packets)
+            {
+                var id = (PacketId)packet.GetCustomAttribute(typeof(PacketId));
+                if (id != null)
+                    types[id.id] = packet;
+            }
+        }
+
+        public static Type GetTypeById(int id)
+        {
+            return types[id];
+        }
+    }
+}
---
+++ src/Merona.Client/Packet.Descriptors.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace Merona.Client
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class PacketId : Attribute
+    {
+        public int id { get; set; }
+        public PacketId(int id)
+        {
+            this.id = id;
+        }
+    }
+}
