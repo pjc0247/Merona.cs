e84bc18d737978dd5e2393548bfbfbee3eaa1bd9
Park June Chul <pjc0247@naver.com>
2015-08-06 17:37:42 +0900

    주석 추가

--- src/Merona/Server.Events.cs
+++ src/Merona/Server.Events.cs
@@ -9,6 +9,9 @@ namespace Merona
 {
     public sealed partial class Server
     {
+        /// <summary>
+        /// 모든 이벤트의 베이스 클래스
+        /// </summary>
 		internal class Event
         {
             internal enum Type
@@ -22,6 +25,9 @@ namespace Merona
             public Type type { get; set; }
         }
 
+        /// <summary>
+        /// IO 쓰레드에서 수행된 Accept를 알려주기 위한 이벤트
+        /// </summary>
 		internal class AcceptEvent : Event
         {
 			public TcpClient client { get; set; }
@@ -32,6 +38,9 @@ namespace Merona
                 this.client = client;
             }
         }
+        /// <summary>
+        /// IO 쓰레드 혹은 Worker 쓰레드에서 세션이 끊어짐을 알려주기 위한 이벤트
+        /// </summary>
 		internal class DisconnectEvent : Event
         {
             public Session session { get; set; }
@@ -42,6 +51,9 @@ namespace Merona
                 this.session = session;
             }
         }
+        /// <summary>
+        /// IO 쓰레드에서 패킷이 수신됨을 알려주기 위한 이벤트
+        /// </summary>
 		internal class RecvPacketEvent : Event
         {
 			public Packet packet { get; set; }
@@ -54,6 +66,13 @@ namespace Merona
                 this.packet = packet;
             }
         }
+        /// <summary>ㄴ
+        /// 안전하지 않은 쓰레드에서 Worker 쓰레드에 함수 호출을 요청하기 위한 이벤트
+        /// </summary>
+        /// <remarks>
+        /// 이 이벤트를 사용하여 안전하지 않은 쓰레드에서 안전한 쓰레드로
+        /// 처리 흐름을 변경할 수 있다.
+        /// </remarks>
 		internal class CallFuncEvent : Event
         {
 			public Action callback { get; set; }
