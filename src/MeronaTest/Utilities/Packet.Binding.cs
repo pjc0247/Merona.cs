5644cfd92b7fdcf401f98133b3ba59ea38270992
Park June Chul <pjc0247@naver.com>
2015-08-06 15:48:41 +0900

    인바인드 테스트 코드 추가

---
+++ src/MeronaTest/Utilities/Packet.Binding.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Merona;
+
+namespace MeronaTest.Utilities
+{
+    [TestClass]
+    public class PacketBindingTest
+    {
+        class ChangeNickname : Packet
+        {
+            [Bind("nickname")]
+            public String nickname;
+        }
+        class QueryNickname : Packet
+        {
+            [Bind("#{nickname}")]
+            public String nickname;
+        }
+        class MySession : Session
+        {
+            public String nickname { get; set; }
+        }
+
+        [TestMethod]
+        public void InBind()
+        {
+            var session = new MySession();
+            var packet = new ChangeNickname();
+
+            packet.nickname = "hello";
+            packet.PreProcess(session);
+
+            Assert.AreEqual(
+                packet.nickname,
+                session.nickname);
+        }
+    }
+}
