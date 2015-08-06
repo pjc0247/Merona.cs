c1b3da21aceec80b3d7ac29cdb54fcf1431625cb
Park June Chul <pjc0247@naver.com>
2015-08-06 15:52:41 +0900

    주석 추가

--- src/Merona/Packet.cs
+++ src/Merona/Packet.cs
@@ -29,6 +29,11 @@ namespace Merona
         }
 
 
+        /// <summary>
+        /// 현재 패킷과, 지정된 세션에 대해서 인바인딩을 수행한다.
+        /// (패킷 필드의 값을 세션의 프로퍼티에 바인딩)
+        /// </summary>
+        /// <param name="session">세션</param>
         private void InBind(Session session)
         {
             var bindFields = GetBindFields(GetType());
@@ -41,6 +46,11 @@ namespace Merona
                 DataBinder.InBind(field.Item1, value, session);
             }
         }
+        /// <summary>
+        /// 현재 패킷과, 지정된 세션에 대해서 아웃바인딩을 수행한다.
+        /// (세션 프로퍼티의 값을 패킷의 필드에 바인딩한다)
+        /// </summary>
+        /// <param name="session">세션</param>
         private void OutBind(Session session)
         {
             var bindFields = GetBindFields(GetType());
@@ -54,6 +64,7 @@ namespace Merona
         }
         /// <summary>
         /// 패킷이 각 서비스들에게 라우팅 되기 전 처리해야 할 작업들을 수행한다.
+        /// 이 작업은 C2S 패킷에만 수행되어야 한다.
         /// </summary>
         /// <param name="session">세션</param>
         internal void PreProcess(Session session)
@@ -70,6 +81,7 @@ namespace Merona
         }
         /// <summary>
         /// 패킷이 클라이언트에게 Send 되기 전 처리해야 할 작업들을 수행한다.
+        /// 이 작업은 S2C 패킷에만 수행되어야 한다.
         /// </summary>
         /// <param name="session">세션</param>
         internal void PostProcess(Session session)
