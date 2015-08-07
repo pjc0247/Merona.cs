73acb57c42c54ac63c48c1b1740e456535b1a583
Park June Chul <pjc0247@naver.com>
2015-08-07 20:04:54 +0900

    DumpCallStack 유틸리티 추가

---
+++ src/Merona/DumpCallStack.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace Merona
+{
+    public class DumpCallStack
+    {
+        public static void Dump()
+        {
+            StackTrace traces = new StackTrace(1);
+            
+            foreach(var trace in traces.GetFrames())
+            {
+                Console.WriteLine("{0} : {1}", trace.GetMethod(), trace.GetFileLineNumber());
+            }
+        }
+    }
+}
