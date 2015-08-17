affe3bf8e6eb44f12eee04653aed3f38eb2ab069
pjc02478 <pjc0247@gmail.com>
2015-08-17 17:06:12 +0900

    커스텀 패킷 디스크립터 인터페이스

---
+++ src/Merona/Packet.CustomDescriptor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Merona
+{
+	public partial class Packet
+	{
+        [AttributeUsage(AttributeTargets.Field)]
+        public class CustomDescriptor : Attribute
+        {
+            protected enum AttachTarget
+            {
+                PreProcess,
+                PostProcess
+            }
+
+            internal protected void OnPreProcess(ref object target)
+            {
+            }
+            internal protected void OnPostProcess(ref object target)
+            {
+            }
+        }
+	}
+}
