d8a213a70524c065eb694ff5cb98e903e6ef2181
pjc02478 <pjc0247@gmail.com>
2015-09-08 10:14:16 +0900

    SafeCollection 테스트 추가

---
+++ src/MeronaTest/Utilities/SafeCollection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Merona;
+
+namespace MeronaTest.Utilities
+{
+    [TestClass]    
+    public class SafeCollection
+    {
+        [TestMethod]
+        public void AutoRemove()
+        {
+            var sc = new SafeCollection<Session>();
+
+            var session = new Session();
+
+            sc.Add(session);
+
+            session.Disconnect();
+
+            Assert.AreEqual(
+                sc.Count,
+                0);
+        }
+
+    }
+}
