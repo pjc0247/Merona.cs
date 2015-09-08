80ae8c6f7afea69a51ec77cdf56cd366b5439d0d
Park June Chul <pjc0247@naver.com>
2015-09-09 01:19:30 +0900

    주석 추가

--- src/Merona/Session.PipelineContext.cs
+++ src/Merona/Session.PipelineContext.cs
@@ -8,6 +8,10 @@ namespace Merona
 {
     public partial class Session
     {
+		/// <summary>
+        /// 클라이언트로부터 요청 패킷을 수신하고,
+        /// 처리해서 전송하기까지의 데이터를 이곳에 저장합니다.
+        /// </summary>
 		public class PipelineContext
         {
 			public Packet request { get; internal set; }
