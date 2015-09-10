6f46322224dba26a61c9e236ab2882529f817b47
Park June Chul <pjc0247@naver.com>
2015-09-11 00:09:11 +0900

    서버 모니터 프로토타입

---
+++ src/Merona/IMonitor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merona
+{
+    // Delegate ?
+
+    interface IExceptionMonitor
+    {
+        void OnUserException(Exception e);
+        void OnSystemException(Exception e);
+    }
+    interface IConnectionMonitor
+    {
+        void OnConnectClient();
+        void OnDisconnectClient();
+
+        void OnConnectCluster();
+        void OnDisconnectCluster();
+    }
+    interface IPacketMonitor
+    {
+        void OnSend(Packet e);
+        void OnRecv(Packet e);
+    }
+}
