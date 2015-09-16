b99ff8184f4788d4548ea20ce515d6fda1683780
Park June Chul <pjc0247@naver.com>
2015-09-17 00:30:35 +0900

    주석 추가

--- src/Merona/Channel.Path.DynamicPathMaker.cs
+++ src/Merona/Channel.Path.DynamicPathMaker.cs
@@ -8,6 +8,13 @@ namespace Merona
     {
         public partial class Path
         {
+            /// <summary>
+            /// DynamicObject 기능을 제공해서 오브젝트 체인으로
+            /// 채널 경로를 만들 수 있는 기능을 제공한다.
+            /// </summary>
+            /// <example>
+            /// var path = Channel.Path.makeDynamicPath.chat.all.fin;
+            /// </example>
             public class DynamicPathMaker : DynamicObject
             {
                 private List<string> tokens { get; set; }
