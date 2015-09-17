18b205bcac7e97df861660a41f4eb609d4e60aeb
Park June Chul <pjc0247@naver.com>
2015-09-18 00:18:41 +0900

    Service.Route 테스트 추가

---
+++ src/MeronaTest/Utilities/Service.Route.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+
+using Merona;
+
+namespace MeronaTest.Utilities
+{
+    [TestClass]
+    public class ServiceRouteTest
+    {
+        class TestPacket : Packet
+        {
+        }
+        class TestService : Service
+        {
+            [Handler("a.b.c.d")]
+            public void OnABCD(Session session, Packet packet)
+            {
+            }
+            [Handler("a.b.c.*")]
+            public void OnABC(Session session, Packet packet)
+            {
+            }
+
+            [Handler(typeof(TestPacket))]
+            public void OnTestPacket(Session session, TestPacket packet)
+            {
+            }
+        }
+
+        [TestMethod]
+        public void RoutingByChannel()
+        {
+            var service = new TestService();
+
+            var routed = service.Route(
+                new Packet()
+                {
+                    channel = "a.b.c.d"
+                });
+
+            Assert.IsTrue(routed);
+        }
+        [TestMethod]
+        public void RoutingByChannelWithWildcard()
+        {
+            var service = new TestService();
+
+            var routed = service.Route(
+                new Packet()
+                {
+                    channel = "a.b.c.xxx"
+                });
+            Assert.IsTrue(routed);
+
+            routed = service.Route(
+                new Packet()
+                {
+                    channel = "a.b.c.zzz"
+                });
+            Assert.IsTrue(routed);
+        }
+
+        [TestMethod]
+        public void RoutingByPacketType()
+        {
+            var service = new TestService();
+
+            var routed = service.Route(
+                new TestPacket());
+
+            Assert.IsTrue(routed);
+        }
+
+        [TestMethod]
+        public void NotRouted()
+        {
+            var service = new TestService();
+
+            var routed = service.Route(
+                new Packet()
+                {
+                    channel = "never.exist.channel"
+                });
+
+            Assert.IsFalse(routed);
+        }
+    }
+}
