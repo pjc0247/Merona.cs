8a9a99135fea70c25786eaca99f85e8f2b0d175e
Park June Chul <pjc0247@naver.com>
2015-09-18 00:04:42 +0900

    ChannelPool.Query가 HashSet을 리턴하도록
    동일한 세션이 여러번 추가될 수 있었음

--- src/Merona/Channel.Pool.cs
+++ src/Merona/Channel.Pool.cs
@@ -46,20 +46,41 @@ namespace Merona
                 }
             }
             
-            public List<Session> Query(Channel.Path path)
+            /// <summary>
+            /// 지정된 채널 경로에 포함되는 모든 세션을 얻어온다.
+            /// [Non-Thread-Safe]
+            /// </summary>
+            /// <param name="path">채널 경로</param>
+            /// <returns>포함된 모든 세션</returns>
+            public HashSet<Session> Query(Channel.Path path)
             {
-                List<Session> results = new List<Session>();
+                HashSet<Session> results = new HashSet<Session>();
 
                 foreach (var channel in pool.Query(path))
-                    results.AddRange(channel.Query());
+                {
+                    foreach (var session in channel.Query())
+                        results.Add(session);
+                }
 
                 return results;
             }
 
+            /// <summary>
+            /// 지정된 채널 경로에 패킷을 브로드캐스팅한다.
+            /// [Non-Thread-Safe]
+            /// </summary>
+            /// <param name="path">채널 경로</param>
+            /// <param name="packet">패킷</param>
             public void Broadcast(String path, Packet packet)
             {
                 Broadcast(new Channel.Path(path), packet);
             }
+            /// <summary>
+            /// 지정된 채널 경로에 패킷을 브로드캐스팅한다.
+            /// [Non-Thread-Safe]
+            /// </summary>
+            /// <param name="path">채널 경로</param>
+            /// <param name="packet">패킷</param>
             public void Broadcast(Channel.Path path, Packet packet)
             {
                 packet.channel = path.path;
