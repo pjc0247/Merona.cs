93ab579baea0ebb2c358c876b2b127fecf5aa942
Park June Chul <pjc0247@naver.com>
2015-09-18 01:06:16 +0900

    주석 추가

--- src/Merona/ValidReference.cs
+++ src/Merona/ValidReference.cs
@@ -26,6 +26,15 @@ namespace Merona
             isValid = false;
         }
 
+        /// <summary>
+        /// 가리키고 있는 객체를 얻어온다.
+        /// 만약 가리키고 있는 객체가 더이상 유효하지 않을 경우
+        /// 익셉션을 발생시킨다.
+        /// </summary>
+        /// <returns>유효할 경우 가리키는 객체</returns>
+        /// <exception cref="InvalidOperationException">
+        /// 가리키는 객체가 더 이상 유효하지 않을 경우
+        /// </exception>
         public T Get()
         {
             if (isValid == false)
